817422554a5824266aad8bdeedb8dd7c8fd0025c
purelove2u <purelovetou@gmail.com>
2020-08-04 22:54:20 +0900

    7강까지

---
+++ spring/src/spring/di/Program.java
@@ -0,0 +1,23 @@
+package spring.di;
+
+import spring.di.entity.Exam;
+import spring.di.entity.MyoungwonExam;
+import spring.di.ui.ExamConsole;
+import spring.di.ui.GridExamConsole;
+import spring.di.ui.InlineExamConsole;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+//		Exam  exam = new MyoungwonExam();
+//		ExamConsole console = new GridExamConsole();
+//		
+//		console.setExam(exam);
+		
+		ExamConsole console = ?;
+		console.print();
+		
+	}
+
+}
