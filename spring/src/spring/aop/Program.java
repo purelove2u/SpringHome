173385ca0303bfd951b094ece3ed4a6686220a1d
purelove2u <59471889+purelove2u@users.noreply.github.com>
2020-08-12 17:28:01 +0900

    21강까지

--- spring/src/spring/aop/Program.java
+++ spring/src/spring/aop/Program.java
@@ -1,5 +1,12 @@
 package spring.aop;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import spring.aop.entity.Exam;
 import spring.aop.entity.MyoungwonExam;
 
@@ -7,9 +14,36 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Exam exam = new MyoungwonExam(1, 1, 1, 1);
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring/aop/setting.xml");
+		//ApplicationContext context = new AnnotationConfigApplicationContext(MyoungwonDiConfig.class);
 		
-		System.out.printf("total is %d\n", exam.total());
+		Exam proxy = (Exam) context.getBean("proxy");
+		
+		System.out.printf("total is %d\n", proxy.total());
+		System.out.printf("avg is %.2f\n", proxy.avg());
+
+		//		Exam exam = new MyoungwonExam(1, 1, 1, 1);
+//		
+//		Exam proxy = (Exam) Proxy.newProxyInstance(MyoungwonExam.class.getClassLoader(), 
+//				new Class[] {Exam.class}, 
+//				new InvocationHandler() {
+//					
+//					@Override
+//					public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+//						
+//						long start = System.currentTimeMillis();
+//						
+//						Object result = method.invoke(exam, args);
+//						
+//						long end = System.currentTimeMillis();
+//						String message = (end - start) + "ms 시간이 걸렸습니다.";
+//						System.out.println(message);
+//						
+//						return result;
+//					}
+//				}
+//				);
+		
 	}
 
 }
--- spring/src/spring/aop/entity/MyoungwonExam.java
+++ spring/src/spring/aop/entity/MyoungwonExam.java
@@ -51,8 +51,20 @@ public class MyoungwonExam implements Exam {
 
 	@Override
 	public int total() {
+//		long start = System.currentTimeMillis();
 		
 		int result = kor + eng + math + com ;
+		
+		try {
+			Thread.sleep(200);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+//		long end = System.currentTimeMillis();
+		
+//		String message = (end - start) + "ms 시간이 걸렸습니다.";
+//		System.out.println(message);
 		
 		return result; 
 	}
